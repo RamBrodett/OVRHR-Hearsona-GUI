a81c880af326e176c19f7a817715884125b91464
Peter David <davidpeterjan@gmail.com>
2025-06-06 16:13:02 +0800

    implemented new user and export chat

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import { useEffect, useRef } from 'react';
 
-import { RotateCcw, HelpCircle, ArrowUp, Clock, Volume2, Activity, Upload, Settings } from 'lucide-react';
+import { RotateCcw, HelpCircle, ArrowUp, Clock, Volume2, Activity, Upload, Settings, Menu, Plus, MessageSquareShare } from 'lucide-react';
 
 function MainApplication() {
   const [activeControl, setActiveControl] = useState(null);
-  
-  {/* Sound Parameters */}
-  const [pitch, setPitch] = useState(440);
-  const [loudness, setLoudness] = useState(-20);
-  const [duration, setDuration] = useState(10);
 
   const [messages, setMessages] = useState([]);
   const [userInput, setUserInput] = useState('');
   const [versions, setVersions] = useState([]);
+  
+  const [history, setHistory] = useState([]);
+
+  {/* Sound Parameters */}
+  const [pitch, setPitch] = useState(440);
+  const [loudness, setLoudness] = useState(-20);
+  const [duration, setDuration] = useState(10);
+  
 
   {/* Autoscroll to the latest chat/version */}
   const chatRef = useRef(null);
@@ -22,11 +25,46 @@ function MainApplication() {
   {/* Start Over Confirmation Panel */}
   const [showConfirm, setShowConfirm] = useState(false);
 
+  useEffect(() => {
+  if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (versionRef.current) {
+      versionRef.current.scrollTop = 0;
+    }
+  }, [versions]);
+
+  {/* Export Chat */}
+  const exportChat = () => {
+    const fileName = prompt("Enter ID:", "chat-export");
+
+    if (!fileName) return;
+
+    const content = history
+      .map(msg => `${msg.role.toUpperCase()}: ${msg.text}`)
+      .join('\n\n');
+
+    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${fileName}.txt`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
+
   const handleSendMessage = async() => {
     if (!userInput.trim()) return;
 
     const newUserMessage = { role: 'user', text: userInput.trim() };
     setMessages((prev) => [...prev, newUserMessage]);
+    setHistory((prev) => [...prev, newUserMessage]);
     setUserInput('');
 
     try{
@@ -52,6 +90,7 @@ function MainApplication() {
 
         const systemMessage = {role: 'assistant', text: responseText};
         setMessages((prev)=> [...prev, systemMessage]);
+        setHistory((prev) => [...prev, systemMessage]);
 
         if(audio_base64 !== null){
           setVersions((prev)=> [...prev,{
@@ -81,18 +120,6 @@ function MainApplication() {
       return null;
     }
 }
-
-  useEffect(() => {
-  if (chatRef.current) {
-      chatRef.current.scrollTop = chatRef.current.scrollHeight;
-    }
-  }, [messages]);
-
-  useEffect(() => {
-    if (versionRef.current) {
-      versionRef.current.scrollTop = 0;
-    }
-  }, [versions]);
 
   return (
     <div className="flex flex-col bg-[var(--background)] p-11 h-screen min-w-screen">
@@ -131,13 +158,25 @@ function MainApplication() {
               </button>
 
               {/* Tooltip */}
+              <div className="relative group inline-block">
+                <button
+                  onClick={() =>
+                    setActiveControl(activeControl === 'info' ? null : 'info')
+                  }
+                  className="bg-[var(--background-2)] text-[var(--font-white)] p-3 rounded-2xl hover:bg-[#3a3a3a] transition"
+                >
+                  <HelpCircle size={22} />
+                </button>
+              </div>
+
+              {/* More Options (New User and Export Chat) */}
               <button
                 onClick={() =>
-                  setActiveControl(activeControl === 'info' ? null : 'info')
+                  setActiveControl(activeControl === 'options' ? null : 'options')
                 }
                 className="bg-[var(--background-2)] text-[var(--font-white)] p-3 rounded-2xl hover:bg-[#3a3a3a] transition"
               >
-                <HelpCircle size={22} />
+                <Menu size={22} />
               </button>
 
             </div>
@@ -158,14 +197,14 @@ function MainApplication() {
             )}
 
             <div
-              className="flex flex-col gap-1 px-4 py-2 overflow-y-auto scroll-smooth"
+              className="flex flex-col gap-2 px-4 py-2 overflow-y-auto scroll-smooth"
               ref={chatRef}
             >
+              {/* Chat Boxes */}
               {messages.map((msg, index) => (
                 <div key={index} className="flex items-start">
-                  {/* Chat Boxes */}
                   <div
-                    className={`px-5 py-4 rounded-xl w-full max-w-[100%] flex items-start gap-4 ${
+                    className={`px-5 py-4 rounded-xl w-full flex items-start gap-4 ${
                       msg.role === 'user'
                         ? 'bg-[var(--background-2)]'
                         : ''
@@ -253,25 +292,35 @@ function MainApplication() {
 
             {/*Pitch Panel*/}
             {activeControl === 'pitch' && (
-              <div className="absolute bottom-9 left-17 translate-y-[-100%] w-64 p-4 bg-[var(--background-3)] text-[var(--font-white)] rounded-2xl shadow-lg z-10">
+              <div className="absolute bottom-9 left-17 translate-y-[-100%] w-64 p-4 bg-[var(--sound-button)] text-[var(--font-white)] rounded-2xl shadow-xl z-10">
                 <div className="flex justify-between text-base mb-2">
                   <span className="text-[var(--font-gray)]">Pitch</span>
                   <span className="text-[var(--font-white)]">{pitch} Hz</span>
                 </div>
-                <input
-                  type="range"
-                  min="80"
-                  max="1000"
-                  value={pitch}
-                  onChange={(e) => setPitch(e.target.value)}
-                  className="w-full accent-white"
-                  />
+                <div className="flex items-center gap-2">
+
+                  <input
+                    type="range"
+                    min="80"
+                    max="1000"
+                    value={pitch}
+                    onChange={(e) => setPitch(e.target.value)}
+                    className="w-full accent-white"
+                    />
+                  <button
+                    onClick={() => setPitch(440)}
+                    className="text-[var(--font-white)] hover:text-[#cccccc] transition"
+                    title="Reset Pitch"
+                  >
+                    <RotateCcw size={18} />
+                  </button>
+                </div>
               </div>
             )}
 
             {/*Loudness Panel*/}
             {activeControl === 'loudness' && (
-              <div className="absolute bottom-9 left-45 translate-y-[-100%] w-64 p-4 bg-[var(--background-3)] text-[var(--font-white)] rounded-2xl shadow-lg z-10">
+              <div className="absolute bottom-9 left-45 translate-y-[-100%] w-64 p-4 bg-[var(--sound-button)] text-[var(--font-white)] rounded-2xl shadow-xl z-10">
                 <div className="flex justify-between text-base mb-2">
                   <span className="text-[var(--font-gray)]">Loudness</span>
                   <span className="text-[var(--font-white)]">{loudness} dB</span>
@@ -289,7 +338,7 @@ function MainApplication() {
 
             {/*Duration Panel*/}
             {activeControl === 'duration' && (
-              <div className="absolute bottom-9 left-82.5 translate-y-[-100%] w-64 p-4 bg-[var(--background-3)] text-[var(--font-white)] rounded-2xl shadow-lg z-10">
+              <div className="absolute bottom-9 left-82.5 translate-y-[-100%] w-64 p-4 bg-[var(--sound-button)] text-[var(--font-white)] rounded-2xl shadow-xl z-10">
                 <div className="flex justify-between text-base mb-2">
                   <span className="text-[var(--font-gray)]">Duration</span>
                   <span className="text-[var(--font-white)]">{duration}s</span>
@@ -305,28 +354,24 @@ function MainApplication() {
               </div>
             )}
 
-            {/*Tooltip Panel*/}
-            {activeControl === 'info' && (
-              <div className="absolute bottom-52 left-155 translate-y-[-100%] w-100 p-5 bg-[var(--background-3)] text-white rounded-2xl shadow-lg z-50">
-                <h3 className="text-lg font-medium text-[var(--font-white)] mb-2">About Hearsona</h3>
-                <p className="text-base text-[var(--font-gray)]">
-                  Hearsona is a tool that helps you create sounds using just words. Type what you want to hear, and the system will turn it into audio for you.
-                  You can also adjust Pitch, Loudness, and Duration to make the sound match your needs.
-                  <br /><br />
-                  It’s designed to be easy to use, with live previews and simple controls, so you can explore and adjust your sound until it feels just right.
-                </p>
-              </div>
-            )}
-
+            {/* Start Over Confirmation Panel */}
             {showConfirm && (
               <div className="fixed inset-0 flex items-center justify-center bg-black/80 z-50">
-                <div className="bg-[var(--background-2)] p-10 rounded-2xl text-center shadow-xl w-[90%] max-w-sm">
-                  <p className="text-[var(--font-white)] text-lg mb-4">
+                <div className="bg-[var(--background-2)] p-10 rounded-2xl text-center shadow-xl w-110">
+                  <p className="text-[var(--font-white)] text-xl font-medium mb-4">
                     Are you sure you want to start over?
+                  </p>
+                  <p className="text-[var(--font-gray)] text-base mb-8">
+                    This will clear all chat history and audio versions.
                   </p>
                   <div className="flex justify-center gap-4">
                     <button
                       onClick={() => {
+                        setHistory((prev) => [
+                          ...prev,
+                          { role: 'system', text: '--- User clicked Start Over ---' },
+                        ]);
+
                         setMessages([]);
                         setVersions([]);
                         setShowConfirm(false);
@@ -343,6 +388,55 @@ function MainApplication() {
                     </button>
                   </div>
                 </div>
+              </div>
+            )}
+
+            {/* Tooltip Panel */}
+            {activeControl === 'info' && (
+              <div className="absolute bottom-52.5 left-134 translate-y-[-100%] w-100 p-5 bg-[var(--sound-button)] text-white rounded-2xl shadow-xl z-50">
+                <h3 className="text-lg font-medium text-[var(--font-white)] mb-2">About Hearsona</h3>
+                <p className="text-base text-[var(--font-gray)]">
+                  Hearsona is a tool that helps you create sounds using just words. Type what you want to hear, and the system will turn it into audio for you.
+                  You can also adjust Pitch, Loudness, and Duration to make the sound match your needs.
+                  <br /><br />
+                  It’s designed to be easy to use, with live previews and simple controls, so you can explore and adjust your sound until it feels just right.
+                </p>
+              </div>
+            )}
+
+            {/* More Options Panel */}
+            {activeControl === 'options' && (
+              <div className="absolute bottom-138.5 left-160 translate-y-[-100%] w-45 p-3 bg-[var(--sound-button)] text-white rounded-2xl shadow-xl z-50">
+                
+                {/* New User */}
+                <button
+                  onClick={() => {
+                      setMessages([]);
+                      setVersions([]);
+                      setHistory([]);
+                      setActiveControl(null);
+                  }}
+                  className="flex items-center gap-2.5 px-3 py-2 w-39 rounded-2xl transition bg-[var(--sound-button)] text-[var(--font-white)] hover:bg-[#3a3a3a]"
+                >
+                  <Plus size={20} />
+                  <span className="text-lg font-medium">
+                    New User
+                  </span>
+                </button>
+
+                {/* Export Chat */}
+                <button
+                  onClick={() => {
+                    exportChat();
+                    setActiveControl(null);
+                  }}
+                  className="flex items-center gap-2.5 px-3 py-2 w-39 mt-2 rounded-2xl transition bg-[var(--sound-button)] text-[var(--font-white)] hover:bg-[#3a3a3a]"
+                >
+                  <MessageSquareShare size={20} />
+                  <span className="text-lg font-medium">
+                    Export Chat
+                  </span>
+                </button>
               </div>
             )}
 
